7d5614e141be010091c1d322f03da98a81a03b25
seanglynn-thrive <93200565+seanglynn-thrive@users.noreply.github.com>
2024-02-02 09:10:04 +0000

    Restructure repo

--- dask-bigquery-io/read.py
+++ dask-bigquery-io/read.py
@@ -1,26 +1,17 @@
 
-import dask.dataframe as dd
-from dask_bigquery import BigQueryTable
-from dask.distributed import Client
-from google.cloud import bigquery
+from dask_bigquery import read_gbq
 
-# # Create a BigQueryTable object
-# table = BigQueryTable(project_id, dataset_id, table_name)
-
-# # Read the BigQuery table as a Dask DataFrame
-# df = table.to_dask()
-
-# # Print the first few rows of the DataFrame
-# print(df.head())
+# Define the project and dataset
 project_id = 'bigquery-public-data'
-dataset_id = 'openaq'
-table_id = 'global_air_quality'
+dataset_id = 'samples'
+table_id = 'wikipedia'
 
-# SQL query to select all columns from the table
-sql_query = f'SELECT * FROM `{project_id}.{dataset_id}.{table_id}`'
-
-# Set up Dask BigQuery reader
-bq_reader = dd.read_bigquery(sql_query, project_id=project_id, dataset_id=dataset_id, table_id=table_id)
+# SQL query to
+ddf = read_gbq(
+    project_id=project_id,
+    dataset_id=dataset_id,
+    table_id=table_id,
+)
 
 # Display Dask DataFrame
-print(bq_reader.head())
+print(ddf.head())
